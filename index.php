a1c6c3a202340109873a086596b489f5d5400ee0
kusrina <55826511+kusrina@users.noreply.github.com>
2020-09-24 00:15:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html>
 <body>
+  
+/*header*/
+header {
+  background-color: #666;
+  padding: 30px;
+  text-align: center;
+  font-size: 35px;
+  color: white;
+}
+  
 <img src="IMG_20180612_110255748_HDR.jpg">
 <h1>My First Heading</h1>
 
