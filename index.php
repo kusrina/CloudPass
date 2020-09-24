4e0a4b4bce4dc50bfc4d19253ed6eb9c36881b3a
kusrina <55826511+kusrina@users.noreply.github.com>
2020-09-24 20:33:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,77 +1,55 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Pass Cloud Citra 035</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-* {
-  box-sizing: border-box;
-}
+  <title>Pass Cloud Citra 035</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <style>
+    * {
+    box-sizing: border-box;
+    }
 
-body {
-  font-family: Arial, Helvetica, sans-serif;
-}
+   body {
+      font-family: montserrat, sans-serif;
+    }
 
-/* Create two columns/boxes that floats next to each other */
-nav {
-  float: left;
-  width: 30%;
-  height: 2400px;
-  background: #433743;
-  padding: 20px;
-  color: white;
-}
-
-article {
-  float: left;
-  padding: 20px;
-  width: 70%;
-  background-color: #BF95BC;
-  height: 250px; 
-}
-
-/* Clear floats after the columns */
-section:after {
-  content: "";
-  display: table;
-  clear: both;
-}
-
-/* Style the footer */
-footer {
-  background-color: #777;
-  padding: 10px;
-  text-align: center;
-  color: white;
-}
-
-/* Responsive layout - makes the two columns/boxes stack on top of each other instead of next to each other, on small screens */
-@media (max-width: 600px) {
-  nav, article {
-    width: 100%;
-    height: auto;
-  }
-}
-</style>
+    p {font-size: 16px; color: white;}
+    .margin {margin-bottom: 45px;}
+    .kiri {
+      float: left;
+      width: 30%;
+      height: 2400px;
+      background: #433743;
+      padding: 0px;
+    }
+    .kanan1 {
+      float: left;
+      padding: 0px;
+      width: 70%;
+      background-color: #7C6C8C;
+      height: 250px; 
+    }
+    .kanan2 {
+      float: left;
+      padding: 270px;
+      width: 70%;
+      background-color: #BF95BC;
+      height: 250px; 
+    }
+  </style>
 </head>
   
-  
+/*===================================================================================================================================================================================/  
   
 <body>
 <section>
-  <nav>
+  <div class= "kiri text-center">
     <h1>Salam. Hi!</h1>
     <h2>My name is Citra Ayu Kusrina,</h2>
     <p>you can call me Citra. I am a student at the State Islamic University of Syarif Hidayatullah Jakarta. My major is informatics engineering, now I'm taking semester 5.</p>
-  </nav>
+  </n>
   
-  <article>
-    <h1>London</h1>
-    <p>London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
-    <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
-  </article>
-</section>
 
 <footer>
   <p>Footer</p>
